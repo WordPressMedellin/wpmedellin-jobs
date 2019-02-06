b5dcd8b43261b412435c59bef2f4e271c4cae9dd
Camilo Mejia <>
2019-02-06 07:58:46 -0500

    Starting files

---
+++ README.md
@@ -0,0 +1,2 @@
+# WP MDE Jobs
+
---
+++ wpmde-jobs.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WP MDE Jobs
+Description:  Allow business and individuals to show their services to the community
+Version:      0.1
+Author:       WordPress Medellín
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'WPMDE_JOBS_FILE', __FILE__ );
+define( 'WPMDE_JOBS_VERSION', '0.1.0.0' );
